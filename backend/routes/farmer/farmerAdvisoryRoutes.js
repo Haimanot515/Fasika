82d33942f9f51490a14b8662568f4f865fd9dcc4
Haimanot515 <haimanotbeka@gmail.com>
2026-01-16 09:30:18 +0300

    Update farmerAdvisoryRoutes.js

--- backend/routes/farmer/farmerAdvisoryRoutes.js
+++ backend/routes/farmer/farmerAdvisoryRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { getFarmerAdvisory } = require("../../controllers/farmer/advisoryController");
+const { getFarmerAdvisory } = require("../../controllers/farmer/farmerAdvisoryController");
 
 // This route will be available at /api/farmer/advisory
 router.get("/advisory", getFarmerAdvisory);
