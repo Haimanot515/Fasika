9b964071a31e7b2bf9346175dc0534a4da3df917
Haimanot515 <haimanotbeka@gmail.com>
2026-01-16 09:43:08 +0300

    Update App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -31,7 +31,7 @@ import AddFarmerListing from "./pages/market/AddFarmerListing";
 import ViewFarmerListing from "./pages/market/ViewFarmerListing";
 import EditFarmerListing from "./pages/market/EditFarmerListing"; 
 import BuyerMarketplace from "./pages/market/BuyerMarketplace";
-import AdvisoryBoard from "./pages/farmer/AdvisoryBoard";
+import AdvisoryBoard from "./pages/Advisory";
 
 // --- ECOSYSTEM & DASHBOARD ---
 import DynamicDashboard from "./pages/dashboard/DynamicDashboard";
