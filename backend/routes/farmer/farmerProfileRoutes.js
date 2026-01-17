4a7071784f831465dc276b9f244f95a0b8d0cb37
Haimanot515 <haimanotbeka@gmail.com>
2026-01-17 15:05:12 +0300

    Update farmerProfileRoutes.js

--- backend/routes/farmer/farmerProfileRoutes.js
+++ backend/routes/farmer/farmerProfileRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const registryController = require('../../controllers/farmer/farmerProfileController');
-const authenticate = require('../../middleware/authenticate'); // Your provided middleware
+const authenticate = require('../../middleware/authMiddleware'); // Your provided middleware
 
 // This route uses your middleware to ensure we have a valid User ID from the cookie
 router.post('/register-onboarding', authenticate, registryController.farmerProfile);
