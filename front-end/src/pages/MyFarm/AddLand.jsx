96aff98fbb233f5f6fd8fc29b2a429a8fa0807fa
Haimanot515 <haimanotbeka@gmail.com>
2026-01-18 12:03:05 +0300

    Update AddLand.jsx

--- front-end/src/pages/MyFarm/AddLand.jsx
+++ front-end/src/pages/MyFarm/AddLand.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import api from "../../api"; 
+import api from "../../api/axios"; 
 
 const AddLand = () => {
     const [loading, setLoading] = useState(false);
